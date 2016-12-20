10aa9ba5368f5eac6fd2279b1c7161ec09585dd0
hangduykhiem <hangduykhiem@gmail.com>
2016-12-20 22:40:20 +0200

    Toolbar for activity, WIP, problem with Bindview in activity

--- app/src/main/kotlin/me/androidkotlinblueprint/common/activity/MainActivity.kt
+++ app/src/main/kotlin/me/androidkotlinblueprint/common/activity/MainActivity.kt
@@ -2,7 +2,9 @@ package me.androidkotlinblueprint.common.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.ViewGroup
+import butterknife.*
 import com.bluelinelabs.conductor.Conductor
 import com.bluelinelabs.conductor.Router
 import com.bluelinelabs.conductor.RouterTransaction
@@ -13,11 +15,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var router: Router
 
+    @BindView(R.id.toolbar) lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+
+        setSupportActionBar(toolbar)
 
         val container = findViewById(R.id.controller_container) as ViewGroup
 
